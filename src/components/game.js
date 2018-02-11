ce4f449a7dd23f0c03f6c175cc5f598b315531e8
Shawn Levin <shawnlevin22@gmail.com>
2018-02-10 23:11:21 -0800

    added robot and reset button

--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getRanNum, userInput, makeGuess } from '../actions';
+import { getRanNum, userInput, makeGuess, numberOfGuesses, reset } from '../actions';
+import '../assets/css/game.css';
+import robot from '../assets/images/penrobot.png';
 
 class Game extends Component {
     componentDidMount() {
@@ -9,14 +11,19 @@ class Game extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Guess Game</h1>
-                <p>Random Number: {this.props.ranNum}</p>
-                <input onChange={this.props.userInput} value={this.props.userGuess} type="number" />
-                <p>{this.props.msg}</p>
-                <br />
-                <button onClick={this.props.makeGuess}>Make Guess</button>
-                {/* <button onClick={this.props.getRanNum}>Get Random Number</button> */}
+            <div className="container">
+                <div className="robot" style={{ backgroundImage: "url(" + robot + ")" }} ></div>
+                <div className="game">
+                    <h1>Guess Game</h1>
+                    {/* <p>Random Number: {this.props.ranNum}</p> */}
+                    <input onChange={this.props.userInput} value={this.props.userGuess} placeholder="Enter Guess" type="number" />
+                    <p>{this.props.msg}</p>
+                    <button onClick={this.props.makeGuess}>Make Guess</button>
+                    <br />
+                    <button onClick={this.props.reset}>Reset Game</button>
+                    <p>Number of guesses: {this.props.numberOfGuesses}</p>
+                    {/* <button onClick={this.props.getRanNum}>Get Random Number</button> */}
+                </div>
             </div>
         )
     }
@@ -27,7 +34,11 @@ function mapStateToProps(state) {
     return {
         ranNum: state.game.randomNumber,
         userGuess: state.game.userGuess,
-        msg: state.game.message
+        msg: state.game.message,
+        numberOfGuesses: state.game.numberOfGuesses,
+        reset: state.game.reset
+
+
     }
 }
-export default connect(mapStateToProps, { getRanNum, userInput, makeGuess })(Game);
+export default connect(mapStateToProps, { getRanNum, userInput, makeGuess, numberOfGuesses, reset })(Game);
--- src/reducers/game_reducer.js
+++ src/reducers/game_reducer.js
@@ -4,17 +4,20 @@ const DEFAULT_STATE = {
     randomNumber: null,
     userGuess: '',
     numberOfGuesses: 0,
-    message: 'Enter a Number between 1 -10'
+    message: 'Enter a Number between 1 -100',
+
 }
 
 export default function (state = DEFAULT_STATE, action) {
     switch (action.type) {
+        case types.RESET:
+            return { ...state, randomNumber: action.payload, userGuess: '', numberOfGuesses: 0, message: 'Enter a Number between 1 -100' };
         case types.GET_RAN_NUMBER:
             return { ...state, randomNumber: action.payload };
         case types.USER_INPUT:
             return { ...state, userGuess: action.payload };
         case types.MAKE_GUESS:
-            let message = 'You got it!!';
+            let message = 'You got it!!!';
             if (state.randomNumber > state.userGuess) {
                 message = "Too Low";
             } else if (state.randomNumber < state.userGuess) {
